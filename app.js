d8b439ceafb77b0e255af9803c9f540a58fe520b
Jose Díaz Moya <diazmoya.daniel@gmail.com>
2025-02-12 17:47:33 -0600

    Desarrollo de primeras funcionalidades

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// Array o arreglo con nombres
+let amigos = [];
+
+// Funcion para agregar amigos
+function agregarAmigos(){
+    let nombre = document.getElementById("amigo")
+}
