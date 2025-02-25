40d85f8c62787d5f693624cbf7d19683e80f8b06
Jose Díaz Moya <diazmoya.daniel@gmail.com>
2025-02-25 16:34:50 -0600

    Agregar función para crear elementos <li> y presentar la lista de nombres de amigos

--- app.js
+++ app.js
@@ -2,19 +2,38 @@
 
 // Array o arreglo con nombres
 let amigos = [];
+let nombreAmigo = document.querySelector('input').value;
+let listaAmigos = document.getElementById('listaAmigos');
 
 // Funcion para agregar amigos - Envía una alerta si no se ingresa un nombre válido, de lo contrario lo suma a la lista 'amigos'
 
 function agregarAmigo(){
-    let nombreAmigo = document.querySelector('input').value;
-
     if (nombreAmigo == ' ') {
         alert('Por favor ingrese un nombre');
     } else {
         amigos.push(nombreAmigo);
         
-        // Se limpian los datos de la caja para ingresar un nombre nuevo
-        document.querySelector('input').value = '';
         console.log(amigos);
+        actualizarLista();
+        limpiarCaja();
+        return;
     }
 }
+
+// Se limpian los datos de la caja para ingresar un nombre nuevo
+
+function limpiarCaja() {
+    document.querySelector('input').value = '';
+}
+
+function actualizarLista() {
+    listaAmigos.innerHTML = "";
+
+    for (let i = 0; i < amigos.length; i++){
+        let nuevosNombres = document.createElement('li');
+        nuevosNombres.textContent = amigos[i];
+        listaAmigos.appendChild(nuevosNombres);
+        return listaAmigos;
+    }
+}
+
