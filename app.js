d9d5b96363ece45ad301331578b539b51adc2937
Jose Díaz Moya <diazmoya.daniel@gmail.com>
2025-02-28 18:36:37 -0600

    Correción función para agregar amigos y de la función para actualizar en pantalla la lista de amigos

--- app.js
+++ app.js
@@ -1,34 +1,39 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+
 // Array o arreglo con nombres
-let nombreAmigo = document.getElementById('amigo');
-let amigos = [];
+let inputAmigos = document.getElementById('amigo');
 let listaAmigos = document.getElementById('listaAmigos');
+let amigos = [];
 
 // Funcion para agregar amigos - Envía una alerta si no se ingresa un nombre válido, de lo contrario lo suma a la lista 'amigos'
 
 function agregarAmigo(){
-    if (!nombreAmigo.value) {
+    console.log(inputAmigos.value)
+    if (!inputAmigos.value) {
         alert('Por favor ingrese un nombre');
         return;
     }
 
-    if (amigos.includes(nombreAmigo.value)) {
-        alert(`Ya ${nombreAmigo.value} ya está en la lista, agregue un segundo nombre o apellido`);
+    if (amigos.includes(inputAmigos.value)) {
+        alert(`Ya ${inputAmigos.value} ya está en la lista, agregue un segundo nombre o apellido`);
         return;
     }
 
-    amigos.push(nombreAmigo.value);
+    amigos.push(inputAmigos.value);
+    inputAmigos.value = '';
+    inputAmigos.focus();
     actualizarLista();
+    return;
 } 
 
 // Función para actualizar la lista de amigos en pantalla
 
 function actualizarLista() {
-    var nuevoNombre = document.createElement('li');
     listaAmigos.innerHTML = "";
 
     for (let i = 0; i < amigos.length; i++){
+        let nuevoNombre = document.createElement('li');
         nuevoNombre.textContent = amigos[i];
         listaAmigos.appendChild(nuevoNombre);
     }
