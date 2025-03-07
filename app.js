e11906e058828f562ea8b3cb1e33bb4266dbabd5
Jose Díaz Moya <diazmoya.daniel@gmail.com>
2025-03-06 18:10:58 -0600

    Comentarios en el código

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 
-// Array o arreglo con nombres
-let inputAmigos = document.getElementById('amigo');
-let listaAmigos = document.getElementById('listaAmigos');
-let amigos = [];
+// Variables
+let sumarAmigo = document.getElementById('amigo'); // Capturamos el input del usuario para sumar un amigo
+let listaAmigos = document.getElementById('listaAmigos'); // Usamos la lista adecuada en el HTML 
+let amigos = []; // Array para almacenar la lista de amigos
 
-// Funcion para agregar amigos - Envía una alerta si no se ingresa un nombre válido, de lo contrario lo suma a la lista 'amigos'
+// Funcion para agregar amigos
 
-function agregarAmigo(){
-    console.log(inputAmigos.value)
-    if (!inputAmigos.value) {
+function agregarAmigo(){ 
+
+    if (!sumarAmigo.value) { // Alerta si no se introduce un nombre en el espacio para sumar amigos
         alert('Por favor ingrese un nombre');
         return;
     }
 
-    if (amigos.includes(inputAmigos.value)) {
-        alert(`Ya ${inputAmigos.value} ya está en la lista, agregue un segundo nombre o apellido`);
+    if (amigos.includes(sumarAmigo.value)) { // Si el nombre ya existe en la lista, no se agrega
+        alert(`Ya ${sumarAmigo.value} ya está en la lista, agregue un segundo nombre o apellido`);
         return;
     }
 
-    amigos.push(inputAmigos.value);
-    inputAmigos.value = '';
-    inputAmigos.focus();
-    actualizarLista();
+    amigos.push(sumarAmigo.value); // Agregamos el nombre al array de amigos
+    sumarAmigo.value = ''; // Limpiamos el input para que el usuario pueda ingresar otro nombre
+    sumarAmigo.focus(); // Volvemos a enfocar el input para que el usuario pueda ingresar otro nombre
+    actualizarLista(); // Actualizamos la lista de amigos
     return;
 } 
 
@@ -37,4 +37,19 @@ function actualizarLista() {
         nuevoNombre.textContent = amigos[i];
         listaAmigos.appendChild(nuevoNombre);
     }
+    return;
+}
+
+// Funcion para sortear amigos
+
+function sortearAmigo() {
+    let sorteo = amigos[Math.floor(Math.random()*10+1)];
+
+    if (amigos = ' ') {
+        alert('No hay amigos para sortear');
+        return;
+    } else {
+        
+    }
+    return sorteo;
 }
